e673322dc4c50b186fb7e7fc17fe5b1fa195a2d2
Olila-png <rmanolila@gmail.com>
2025-05-17 17:06:04 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC-lyNTFLutRn6s3htnEmZBOqYXSPUenog",
+  authDomain: "olila-crud.firebaseapp.com",
+  databaseURL: "https://olila-crud-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "olila-crud",
+  storageBucket: "olila-crud.firebasestorage.app",
+  messagingSenderId: "633631571676",
+  appId: "1:633631571676:web:1e1f454a29840d33d83e19"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
